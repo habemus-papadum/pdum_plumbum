08210406bbd8fc30aa27b4fd871289e8d82df124
Nehal Patel <nehal@alum.mit.edu>
2025-11-02 16:55:14 -0500

    fix exports

--- src/pdum/plumbum/__init__.py
+++ src/pdum/plumbum/__init__.py
@@ -1,10 +1,17 @@
 """A plumbing syntax for Python"""
 
+from . import aiterops as _aiterops_module
+from . import iterops as _iterops_module
+from .aiterops import AsyncMapper, AsyncPredicate, aiter, aselect, async_iter_operator, awhere
 from .async_pipeline import AsyncPb, AsyncPbFunc, AsyncPbPair, apb, ensure_async_pb
 from .core import Pb, PbFunc, PbPair, pb
 from .iterops import select, where
 
 __version__ = "0.4.0-alpha"
+
+# Re-export module objects for convenience
+iterops = _iterops_module
+aiterops = _aiterops_module
 
 __all__ = [
     "__version__",
@@ -20,4 +27,11 @@ __all__ = [
     "iterops",
     "select",
     "where",
+    "aiterops",
+    "aiter",
+    "aselect",
+    "awhere",
+    "AsyncMapper",
+    "AsyncPredicate",
+    "async_iter_operator",
 ]
