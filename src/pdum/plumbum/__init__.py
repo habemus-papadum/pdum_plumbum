35eaf7998560577db71e9a0f1162021583547b01
Nehal Patel <nehal@alum.mit.edu>
2025-11-02 16:49:34 -0500

    Bump to 0.4.0-alpha

--- src/pdum/plumbum/__init__.py
+++ src/pdum/plumbum/__init__.py
@@ -4,7 +4,7 @@ from .async_pipeline import AsyncPb, AsyncPbFunc, AsyncPbPair, apb, ensure_async
 from .core import Pb, PbFunc, PbPair, pb
 from .iterops import select, where
 
-__version__ = "0.3.0"
+__version__ = "0.4.0-alpha"
 
 __all__ = [
     "__version__",
